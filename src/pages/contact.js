2579c83a39587d1b563c72362249921a50dd3810
Ryan Taylor <rctaylor123@gmail.com>
2021-09-10 08:55:26 -0500

    outbound link for google analytics

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 
 import Card from "react-bootstrap/Card";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const ContactPage = () => (
   <Layout pageInfo="contact">
@@ -13,7 +14,7 @@ const ContactPage = () => (
       <Card.Body>
         <h1>Contact Us</h1>
         <p>Fill out the form below and someone will get back to you.</p>
-        <p>You can also <a href="https://facebook.com/crosswaycelina">find us on Facebook!</a></p>
+        <p>You can also <OutboundLink href="https://facebook.com/crosswaycelina">find us on Facebook!</OutboundLink></p>
 
         <ContactUsForm></ContactUsForm>
       </Card.Body>
